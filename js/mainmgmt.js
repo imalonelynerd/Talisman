c3d8725b31a217767b335abb3f7416c42bb80ad9
Nerd <imalonelynerd@gmail.com>
2023-06-17 22:27:29 +0200

    Fixed number management system

--- js/mainmgmt.js
+++ js/mainmgmt.js
@@ -1,35 +1,65 @@
-function pos() {
+function add(isneg) {
     let lpcompt = document.getElementById("lp");
-    lpcompt.value -= lpcompt.getAttribute("step") * -1;
-    if (lpcompt.value > parseInt(lpcompt.getAttribute("max"))) {
-        lpcompt.value = lpcompt.getAttribute("max");
+    let addval = parseInt(lpcompt.getAttribute("step"));
+    addval *= isneg ? 1 : -1;
+    lpcompt.value -= addval;
+    return reeditLP();
+}
+
+function reeditLP() {
+    reeditVal("lp");
+}
+
+function reeditVal(id) {
+    let lpcompt = document.getElementById(id);
+    let minv = parseInt(lpcompt.getAttribute("min"));
+    let maxv = parseInt(lpcompt.getAttribute("max"));
+    if (lpcompt.value < minv) {
+        lpcompt.value = minv;
+        return false;
+    }
+    if (lpcompt.value > maxv) {
+        lpcompt.value = maxv;
+        return false;
     }
     return true;
 }
 
-function neg() {
-    let lpcompt = document.getElementById("lp");
-    lpcompt.value -= lpcompt.getAttribute("step");
-    if (lpcompt.value < 0) {
-        lpcompt.value = 0;
-    }
-    return true;
-}
+document.getElementById("lp").addEventListener("blur", reeditLP);
 
 document.getElementById("timerconf").style.display = "none";
 document.getElementById("nexusconf").style.display = "none";
 
-document.getElementById("chrono").addEventListener("click",function () {
+document.getElementById("chrono").addEventListener("click", function () {
     document.getElementById("timerconf").style.display = "none";
     document.getElementById("nexusconf").style.display = "none";
 });
 
-document.getElementById("timer").addEventListener("click",function () {
+document.getElementById("timer").addEventListener("click", function () {
     document.getElementById("timerconf").style.display = "flex";
     document.getElementById("nexusconf").style.display = "none";
 });
 
-document.getElementById("nexus").addEventListener("click",function () {
+document.getElementById("nexus").addEventListener("click", function () {
     document.getElementById("timerconf").style.display = "none";
     document.getElementById("nexusconf").style.display = "flex";
 });
+
+function setup() {
+    let pl1 = document.getElementById("pl1").value;
+    if (pl1 === "" || pl1 === undefined) {
+        pl1 = "Player 1";
+    }
+    let pl2 = document.getElementById("pl2").value;
+    if (pl2 === "" || pl2 === undefined) {
+        pl2 = "Player 1";
+    }
+    let lp = document.getElementById("lp");
+    let lpv = lp.value;
+    let lpmx = lp.getAttribute("max");
+    let lpmn = lp.getAttribute("min");
+    if (lpv < lpmn || lpv > lpmx) {
+        alert("Error : incorrect LP values. Please, try again.");
+        return false;
+    }
+}
